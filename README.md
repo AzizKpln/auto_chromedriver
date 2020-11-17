e0d5238769c09d404ee717a44c91db33bb7b6b48
whistleye <AzizKpln@protonmail.com>
2020-11-17 07:40:12 -1000

    v1.0.0

---
+++ README.md
@@ -0,0 +1,6 @@
+# INSTALLATION
+pip3 install auto_chromedriver
+
+# USAGE
+import auto_chromedriver as ac
+ac.install()
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name="auto_chromedriver",
+    version="1.0.0",
+    packages=["auto_chromedriver"],
+    install_requires=["requests","subprocess","string"],
+    url="https://github.com/AzizKpln/auto_chromedriver",
+    license="GPL-3.0",
+    author="Aziz Kaplan",
+    author_email="AzizKpln@protonmail.com",
+    description="""
+                Auto chromedriver installer. Most of selenium projects are not opening because of chromedriver version issue.
+		with 2 codes you can fix that error without making an effort.
+                
+		For the usage and more information check out the README.""",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
